d003374f8e4d26acbdeedfa1e770e0c63973513f
Isaac <isaac.ewing3@gmail.com>
2021-06-30 15:27:37 -0600

    basic homepage working with login and logout functions made

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
+import HomePage from "./HomePage.js";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const KEY = 'TOKEN';
+
+export default class App extends React.Component {
+    state = {
+      token: localStorage.getItem(KEY)
+    }
+
+    login = (userToken) => {
+      this.setState({ token: userToken })
+      localStorage.setItem(KEY, userToken) 
+    }
+
+    logout = () => {
+      this.setState({ token: '' })
+      localStorage.setItem('TOKEN', '')
+    }
+
+    render() {
+      return (
+        <Router>
+          <div>
+            <Link to='/'>Home Page</Link>
+          </div>
+          <Switch>
+            <Route path='/' exact render={(routerProps) => <HomePage {...routerProps} />} />
+          </Switch>
+        </Router>
+      )
+    }
 }
 
-export default App;
---
+++ src/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class HomePage extends React.Component {
+    render() {
+        return (
+            <div>
+                The best home page, hands down
+            </div>
+        )
+    }
+}
